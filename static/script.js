23645880ca35c8891990d0a7df005ce5d1955920
vbbvragens <ardaofarda79@gmail.com>
2025-01-24 19:37:56 +0100

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,20 @@
+const toggleButton = document.getElementById('darkModeToggle');
+    const body = document.body;
+    const header = document.querySelector('header');
+    const main = document.querySelector('main');
+    const footer = document.querySelector('footer');
+    const sections = document.querySelectorAll('section');
+    const h2s = document.querySelectorAll('h2');
+    const h3s = document.querySelectorAll('h3');
+    const buttons = document.querySelectorAll('button');
+
+    toggleButton.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+        header.classList.toggle('dark-mode');
+        main.classList.toggle('dark-mode');
+        footer.classList.toggle('dark-mode');
+        sections.forEach(section => section.classList.toggle('dark-mode'));
+        h2s.forEach(h2 => h2.classList.toggle('dark-mode'));
+        h3s.forEach(h3 => h3.classList.toggle('dark-mode'));
+        buttons.forEach(button => button.classList.toggle('dark-mode'));
+    });
